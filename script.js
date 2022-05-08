b2ed2633c03a952dd73a13f316c57fe97551d95d
danielabadgamero <danielabadgamero@gmail.com>
2022-05-08 10:41:28 +0200

    Added game logic and functions to test for winners

--- script.js
+++ script.js
@@ -2,6 +2,96 @@ const board = document.querySelector(".board");
 const cells = [];
 const playAgainstCheckboxes = [...document.querySelectorAll("input[type='radio']")];
 const form = document.querySelector("form")
+const startButton = form.querySelector("button");
+const winnerBoard = document.querySelector("#winner-board")
+
+let player1Name;
+let player2Name;
+let mode;
+let occupiedCells = [];
+let winner;
+
+const start = (() => {
+    occupiedCells = [];
+    const computer = () => {
+        document.getElementById("player1-turn").textContent = "You (X)";
+        document.getElementById("player2-turn").textContent = "Computer (O)";
+        if (Math.floor(Math.random() * 2)) {
+            document.getElementById("player1-turn").classList.add("turn");
+        } else {
+            document.getElementById("player2-turn").classList.add("turn");
+        }
+    }
+    const player = () => {
+        document.getElementById("player1-turn").textContent = player1Name + "(X)";
+        document.getElementById("player2-turn").textContent = player2Name + "(O)";
+        if (Math.floor(Math.random() * 2)) {
+            document.getElementById("player1-turn").classList.add("turn");
+        } else {
+            document.getElementById("player2-turn").classList.add("turn");
+        }
+    };
+    return {
+      computer,
+      player,
+    };
+  })();
+
+function nextTurn() {
+    document.querySelector("#player1-turn").classList.toggle("turn");
+    document.querySelector("#player2-turn").classList.toggle("turn");
+}
+
+function randomCellNumber() {
+    return Math.floor(Math.random() * 9)
+}
+
+function computerMove(occupiedCells) {
+    let randomCell = randomCellNumber();
+    if (occupiedCells.length >= 9) {
+        return "Ties!";
+    }
+    if (occupiedCells.indexOf(randomCell) === -1) {
+        document.getElementById(randomCell).textContent = "O";
+        occupiedCells.push(randomCell);
+    } else {
+        computerMove(occupiedCells);
+    }
+}
+
+function checkWinner() {
+    for (let i = 0; i <= 2; i++) { // checks for columns
+        if (cells[i].textContent === "X" && cells[i + 3].textContent === "X" && cells[i + 6].textContent === "X") {
+            return "player1";
+        } else if(cells[i].textContent === "O" && cells[i + 3].textContent === "O" && cells[i + 6].textContent === "O") {
+            return "player2";
+        }
+    }
+    for (let j = 0; j <= 6; j += 3) { // checks for rows
+        if (cells[j].textContent === "X" && cells[j + 1].textContent === "X" && cells[j + 2].textContent === "X") {
+            return "player1";
+        } else if (cells[j].textContent === "O" && cells[j + 1].textContent === "O" && cells[j + 2].textContent === "O") {
+            return "player";
+        }
+    }
+    if (cells[0].textContent === "X" && cells[4].textContent === "X" && cells[8].textContent === "X") { // checks for top-left to bottom-right diagonal
+        return "player1";
+    } else if (cells[0].textContent === "O" && cells[4].textContent === "O" && cells[8].textContent === "O") {
+        return "player2";
+    }
+    if (cells[2].textContent === "X" && cells[4].textContent === "X" && cells[7].textContent === "X") { // checks for top-right to bottom-left diagonal
+        return "player1";
+    } else if (cells[2].textContent === "O" && cells[4].textContent === "O" && cells[7].textContent === "O") {
+        return "player2";
+    }
+}
+
+function showWinner() {
+    winnerBoard.classList.add("winner");
+    if (mode === "computer") {
+        winnerBoard.querySelector("span#winner").textContent = winner;
+    }
+}
 
 for (let i = 0; i <= 8; i++) {
     cells.push(document.createElement(`div`));
@@ -16,4 +106,55 @@ playAgainstCheckboxes[0].addEventListener("change", (e) => {
 
 playAgainstCheckboxes[1].addEventListener("change", (e) => {
         form.querySelector("div.empty").classList.add("names");
+})
+
+startButton.addEventListener("click", () => {
+    if (!playAgainstCheckboxes[0].checked && !playAgainstCheckboxes[1].checked) {
+        alert("No opponent selected");
+    } else if (playAgainstCheckboxes[1].checked) {
+        if (!document.getElementById("player1").value || !document.getElementById("player2").value) {
+            document.getElementById("player1").style.borderColor = "red";
+            document.getElementById("player2").style.borderColor = "red";
+        } else {
+            player1Name = document.getElementById("player1").value;
+            player2Name = document.getElementById("player2").value;
+            document.querySelector("div.empty").classList.remove("prompt-ask");
+            start.player();
+            mode = "player";
+        }
+    } else {
+        document.querySelector("div.empty").classList.remove("prompt-ask");
+        start.computer();
+        mode = "computer";
+    }
+})
+
+cells.forEach(cell => {
+    cell.addEventListener("click", (e) => {
+        if (!e.target.textContent) {
+            if (mode === "computer") {
+                if (document.getElementById("player1-turn").classList.contains("turn")) {
+                    e.target.textContent = "X"
+                    occupiedCells.push(parseInt(e.target.id));
+                    computerMove(occupiedCells);
+                } else {
+                    computerMove(occupiedCells);
+                }
+            } else {
+                if (document.getElementById("player1-turn").classList.contains("turn")) {
+                    e.target.textContent = "X"
+                    nextTurn();
+                } else {
+                    e.target.textContent = "O"
+                    nextTurn();
+                }
+            }
+        } else {
+            alert("Cell is not free");
+        }
+        winner = checkWinner();
+        if (winner) {
+            showWinner();
+        }
+    })
 })
